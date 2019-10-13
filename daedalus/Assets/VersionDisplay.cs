9b4a279c1625b90c95109562ef27485edaeda843
Tania A <ithiliel@gmail.com>
2019-10-13 17:00:18 +0200

    Version # to show on Intro screen.

---
+++ daedalus/Assets/VersionDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionDisplay : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI _label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _label = GetComponent<TMPro.TextMeshProUGUI>();
+        if(_label != null)
+        {
+            _label.text = $"v{Application.version}";
+        }
+    }
+}
